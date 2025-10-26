ce30efce2bf796f14da15e7dee9827dfdbd66e4c
Fox <thefoxjones@gmail.com>
2025-10-26 12:01:23 -0500

    USE TYPESCRIPT

---
+++ dist/index.d.ts
@@ -0,0 +1,46 @@
+/** biome-ignore-all lint/complexity/noStaticOnlyClass: BRO ITS A D.TS FILE */
+type COMPATIBILITY_MODE_PARAM = "none" | "ie5" | "netscape" | "opera_presto";
+
+export default class falsejs {
+
+  static COMPATIBILITY_MODE: {
+    readonly NONE: "none",
+    readonly IE5: "ie5",
+    readonly NETSCAPE: "netscape",
+    readonly OPERA_PRESTO: "opera_presto"
+  }
+
+  /**
+   * Returns false from the given parameters.
+   *
+   * @param {"yes"|"no"} loggingEnabled - Indicates whether logging should be enabled.
+   * @param {"yes"|"no"} shouldDoSomethingAsync - A pointless option that indicates whether something should be done asynchronously that just waits 200ms before saying "Did something async"
+   * @param {"yes"|"no"} shouldDoSomethingAsyncWithIsTenThousand - Indicates whether something should be done asynchronously when checking the self equality of 10,000 using the isTenThousand module (credits to james-work-account)
+   * @param {"yes"|"no"} disableAprilFoolsSideEffects - Indicates whether April Fools side effects should be disabled.
+   * @param {"yes"|"no"} definitelyDisableAprilFoolsSideEffects - Indicates whether April Fools side effects should be definitely disabled.
+   * @param {"yes"|"no"} strictDisableAprilFoolsSideEffectsCheck - Indicates whether strict checking for disabling April Fools side effects should be enabled.
+   * @param {"none"|"ie5"|"netscape"|"opera_presto"} compatibilityMode - The compatibility mode for various legcay browser environments.
+   * @returns {boolean} - The calculated boolean value 'false'.
+   */
+  static False(
+    loggingEnabled?: "yes" | "no",
+    shouldDoSomethingAsync?: "yes" | "no",
+    shouldDoSomethingAsyncWithIsTenThousand?: "yes" | "no",
+    disableAprilFoolsSideEffects?: "yes" | "no",
+    definitelyDisableAprilFoolsSideEffects?: "yes" | "no",
+    strictDisableAprilFoolsSideEffectsCheck?: "yes" | "no",
+    compatibilityMode?: COMPATIBILITY_MODE_PARAM
+  ): boolean
+
+  /**
+   * Checks if a given value is false.
+   *
+   * @param {any} value - The value to be checked.
+   * @returns {boolean} - True if the value is false, false otherwise.
+   */
+
+  static isFalse(value: any, loggingEnabled?: "yes" | "no"): boolean
+
+  static injectIntojQuery(): void
+  static expressMiddleware(req: any, res: any, next: any): void
+}
--- index.test.js
+++ index.test.js
@@ -1,69 +1,82 @@
 function doTests(testName, fjs) {
-	const { False, isFalse, expressMiddleware, injectIntojQuery } = fjs
-	const assert = require("assert-fn")
-	const attempt = require("attempt-statement")
-	const n0p3 = require("n0p3")
-	const clc = require("cli-color")
-	const leftPad = require("left-pad")
-	const lpi = require("the-number-ten")
+  const { False, isFalse, expressMiddleware, injectIntojQuery } = fjs
+  const assert = require("assert-fn")
+  const attempt = require("attempt-statement")
+  const n0p3 = require("n0p3")
+  const clc = require("cli-color")
+  const leftPad = require("left-pad")
+  const lpi = require("the-number-ten")
 
-	const FALSE = 1 == 3
-	const TRUE = 1 == 1
+  const FALSE = 1 == 3
+  const TRUE = 1 == 1
 
-	attempt(() => {
-		const combinations = [
-			["yes", "yes", "yes", "yes", "yes", "no", "no"],
-			["yes", "yes", "no", "yes", "yes", "no", "no"],
-			["yes", "no", "yes", "yes", "yes", "no", "no"],
-			["yes", "no", "no", "yes", "yes", "no", "no"],
-			["no", "yes", "yes", "yes", "yes", "no", "no"],
-			["no", "yes", "no", "yes", "yes", "no", "no"],
-			["no", "no", "yes", "yes", "yes", "no", "no"],
-			["no", "no", "no", "yes", "yes", "no", "no"],
-		]
+  attempt(() => {
+    const BOOL_OPTIONS = ["yes", "no"]
 
-		// False function testing
-		combinations.forEach((params) => {
-			assert(
-				False(...params) === FALSE,
-				`False(${params.join(", ")}) did not return false`,
-			)
-		})
+    const COMPAT_OPTIONS = ["none", "ie5", "netscape", "opera_presto"]
 
-		assert(isFalse(FALSE) === TRUE, "isFalse(false) did not return true")
-		assert(isFalse(TRUE) === FALSE, "isFalse(true) did not return false")
-		global.jQuery = require("jquery")
-		injectIntojQuery()
-		assert(
-			jQuery.False == False && jQuery.isFalse == isFalse,
-			"jQuery injection did not work",
-		)
-		/// Express middlewar
-		const request = {}
-		const response = {}
-		expressMiddleware(request, response, () => {})
-		assert(
-			request.False == False,
-			"Express middleware injection to request did not work",
-		)
-		assert(
-			request.isFalse == isFalse,
-			"Express middleware injection to request did not work",
-		)
-	})
-		.rescue((error) => {
-			console.log(clc.red(leftPad(`× TESTS FAILED FOR ${testName}!!!!!`, lpi)))
-			throw error
-		})
-		.else(() => {
-			setTimeout(function () {
-				console.log(
-					clc.green(leftPad(`✓ TESTS PASSED FOR ${testName}!!!`, lpi)),
-				)
-			}, 5000)
-		})
-		.ensure(n0p3)
-		.end()
+    const ALL_PARAMETER_SETS = [BOOL_OPTIONS, BOOL_OPTIONS, BOOL_OPTIONS, BOOL_OPTIONS, BOOL_OPTIONS, BOOL_OPTIONS, COMPAT_OPTIONS]
+
+    function generateCombinations(sets) {
+      if (!sets || sets.length === 0) {
+        return [[]]
+      }
+
+      let result = sets[0].map((item) => [item])
+
+      for (let i = 1; i < sets.length; i++) {
+        const currentSet = sets[i]
+        const nextResult = []
+
+        for (const existingCombination of result) {
+          for (const item of currentSet) {
+            nextResult.push([...existingCombination, item])
+          }
+        }
+        result = nextResult
+      }
+
+      return result
+    }
+
+    const allCombinations = generateCombinations(ALL_PARAMETER_SETS)
+		const combinations = allCombinations.filter(combination => {
+    const isFourthYes = combination[3] === "yes";
+    const isFifthYes = combination[4] === "yes";
+    const isSixthYes = combination[5] === "yes";
+
+    return !(isFourthYes && isFifthYes && isSixthYes);
+});
+
+
+    // False function testing
+    combinations.forEach((params) => {
+      assert(False(...params) === FALSE, `False(${params.join(", ")}) did not return false`)
+    })
+
+    assert(isFalse(FALSE) === TRUE, "isFalse(false) did not return true")
+    assert(isFalse(TRUE) === FALSE, "isFalse(true) did not return false")
+    global.jQuery = require("jquery")
+    injectIntojQuery()
+    assert(jQuery.False == False && jQuery.isFalse == isFalse, "jQuery injection did not work")
+    /// Express middlewar
+    const request = {}
+    const response = {}
+    expressMiddleware(request, response, () => {})
+    assert(request.False == False, "Express middleware injection to request did not work")
+    assert(request.isFalse == isFalse, "Express middleware injection to request did not work")
+  })
+    .rescue((error) => {
+      console.log(clc.red(leftPad(`× TESTS FAILED FOR ${testName}!!!!!`, lpi)))
+      throw error
+    })
+    .else(() => {
+      setTimeout(function () {
+        console.log(clc.green(leftPad(`✓ TESTS PASSED FOR ${testName}!!!`, lpi)))
+      }, 5000)
+    })
+    .ensure(n0p3)
+    .end()
 }
 
-doTests("FALSEJS MAIN FILE index.js TESTS", require("./index").default)
+doTests("FALSEJS MAIN FILE index.js TESTS", require("./dist/index").default)
